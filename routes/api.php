95953e45a4780fec308063316557f8787a45da1f
salvador.perez <salvador.perez@euroinnova.tech>
2024-08-30 10:00:20 +0200

    Basic Authentication token implementation

--- routes/api.php
+++ routes/api.php
@@ -2,13 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('/tokens/create', function (Request $request) {
-    $token = $request->user()->createToken($request->token_name);
- 
-    return ['token' => $token->plainTextToken];
-});
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::post('/register', [AuthController::class, 'register']);
