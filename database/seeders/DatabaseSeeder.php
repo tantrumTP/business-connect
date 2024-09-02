421ca3847850d210153ab10acde9d15488ff5c97
salvador.perez <salvador.perez@euroinnova.tech>
2024-09-02 16:00:03 +0200

    Business, Product and Service factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Business;
+use App\Models\Product;
+use App\Models\Service;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,9 +18,9 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        Business::factory(10)
+        ->has(Product::factory(3))
+        ->has(Service::factory(2))
+        ->create();
     }
 }
