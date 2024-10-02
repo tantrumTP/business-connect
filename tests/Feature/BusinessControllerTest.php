4f3cb35237900c360d4fe77ee3724dc8b400d6ad
salvador.perez <salvador.perez@euroinnova.tech>
2024-10-02 16:44:49 +0200

    Feat(HandleMediaTrait.php): modify to use with test Feat(BusinessControllerTest.php): test_create_business method complete

--- tests/Feature/BusinessControllerTest.php
+++ tests/Feature/BusinessControllerTest.php
@@ -2,8 +2,7 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 use App\Models\User;
 use App\Models\Business;
@@ -15,7 +14,7 @@ use Laravel\Sanctum\Sanctum;
 class BusinessControllerTest extends TestCase
 {
 
-    use RefreshDatabase, HandleMediaTrait;
+    use DatabaseTransactions, HandleMediaTrait;
 
     private function getUser()
     {
@@ -30,7 +29,7 @@ class BusinessControllerTest extends TestCase
         $user = $this->getUser();
         Sanctum::actingAs($user, ['*']);
 
-        $media = UploadedFile::fake()->image('logo.jpg');
+        $media = UploadedFile::fake()->image('image.jpg');
         $businessData = [
             'name' => 'Business Test',
             'description' => 'Business created for automatic tests',
@@ -53,7 +52,7 @@ class BusinessControllerTest extends TestCase
 
         $response = $this->postJson('/api/businesses', $businessData);
 
-
+        // Check status response and structure of data
         $response->assertStatus(200)
             ->assertJsonStructure([
                 'success',
@@ -72,17 +71,28 @@ class BusinessControllerTest extends TestCase
                 'message'
             ]);
 
-        $business = Business::first();
-        $this->assertDatabaseHas('media', [
-            'mediaable_type' => Business::class,
-            'mediaable_id' => $business->id,
-            'type' => 'image',
-            'caption' => 'Example image'
+        $business = Business::latest()->first();
+
+        // Check business created sucessfully
+        $this->assertDatabaseHas('businesses', [
+            'id' => $business->id,
+            'name' => 'Business Test',
+            'email' => 'test@example.com'
         ]);
 
+        //Check file is associated with business
+        $this->assertDatabaseHas('media', [
+            'type' => 'image',
+            'caption' => 'Example image',
+            'mediaable_type' => Business::class,
+            'mediaable_id' => $business->id,
+        ]);
         $media = $business->media->first();
+
+        // Check the file was stored
         Storage::disk('public')->assertExists($media->file_path);
 
+        // Check business is associated with the correct user
         $this->assertEquals($user->id, $business->user_id);
     }
 }
