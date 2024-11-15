75de45bf984701986179644bc8363e1e66bed57f
salvador.perez <salvapere@gmail.com>
2024-11-15 12:03:00 +0100

    Schedule command

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::command(PermanentlyDeleteOldRecords::class)->everyTenSeconds();
+Schedule::command(PermanentlyDeleteOldRecords::class)->daily();
